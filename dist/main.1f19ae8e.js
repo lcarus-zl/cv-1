d0fcb4deb468e2aebcddb61e240ff95bc476a163
lcarus-zl <zl351889@163.com>
2022-03-03 20:44:50 +0800

    ./

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -178,7 +178,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "14880" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "3664" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
